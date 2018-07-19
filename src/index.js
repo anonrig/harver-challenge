7de46038b5b499dc2748b6d39e7d8a78ed540408
Yagiz Nizipli <yagiznizipli@me.com>
2018-07-19 13:24:22 +0300

    :sparkles: Implement task 5 with Express

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-const { getRandomWordSync, getRandomWord } = require('word-maker');
+const { getRandomWord } = require('word-maker');
+const express = require('express');
+const app = express();
 
+app.get('/', async (req, res) => {
+  try {
+    let response = [];
+    for (let i = 1; i <= 100; i++) {
+      let keyword = '';
 
+      try {
+        keyword = await getRandomWord({withErrors: true});
 
-
-const App = async () => {
-  for (let i = 1; i <= 100; i++) {
-    let keyword = '';
-
-    try {
-      keyword = await getRandomWord({withErrors: true});
-
-      if (i % 15 == 0) {
-        keyword = 'FizzBuzz';
-      } else if (i % 3 == 0) {
-        keyword = 'Fizz';
-      } else if (i % 5 == 0) {
-        keyword = 'Buzz';
+        if (i % 15 == 0) {
+          keyword = 'FizzBuzz';
+        } else if (i % 3 == 0) {
+          keyword = 'Fizz';
+        } else if (i % 5 == 0) {
+          keyword = 'Buzz';
+        }
+      } catch (error) {
+        keyword = 'Doh!';
       }
-    } catch (error) {
-      keyword = 'Doh!';
+
+      response.push({id: i, keyword});
     }
 
-    console.log(`${i}: ${keyword}`);
+    res.json(response);
+  } catch(error) {
+    res.status(500).json({error});
   }
-};
+});
 
-
-App()
-  .then(() => console.info('Finished'))
-  .catch(error => console.error(`Error occurred ${error.message}`));
+app.listen(3000, () => console.info(`Listening server on port 3000`));
