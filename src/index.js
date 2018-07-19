26358164911af2482a0f8f6f4ef20344157a5469
Yagiz Nizipli <yagiznizipli@me.com>
2018-07-19 13:16:27 +0300

    :sparkles: Implement FizzBuzz challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const { getRandomWordSync, getRandomWord } = require('word-maker');
 
-for (let i = 0; i < 100; i++) {
-  console.log(`${i+1}: ${getRandomWordSync()}`);
+for (let i = 1; i <= 100; i++) {
+  let keyword = getRandomWordSync();
+
+  if (i % 15 == 0) {
+    keyword = 'FizzBuzz';
+  } else if (i % 3 == 0) {
+    keyword = 'Fizz';
+  } else if (i % 5 == 0) {
+    keyword = 'Buzz';
+  }
+
+  console.log(`${i}: ${keyword}`);
 }
