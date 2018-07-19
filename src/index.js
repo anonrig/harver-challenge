b29a4bedb2273c16776887deb7c484ef0c82f2b6
Yagiz Nizipli <yagiznizipli@me.com>
2018-07-19 13:20:11 +0300

    :sparkles: Implement task 4

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 
 const App = async () => {
   for (let i = 1; i <= 100; i++) {
-    let keyword = await getRandomWord();
+    let keyword = '';
 
-    if (i % 15 == 0) {
-      keyword = 'FizzBuzz';
-    } else if (i % 3 == 0) {
-      keyword = 'Fizz';
-    } else if (i % 5 == 0) {
-      keyword = 'Buzz';
+    try {
+      keyword = await getRandomWord({withErrors: true});
+
+      if (i % 15 == 0) {
+        keyword = 'FizzBuzz';
+      } else if (i % 3 == 0) {
+        keyword = 'Fizz';
+      } else if (i % 5 == 0) {
+        keyword = 'Buzz';
+      }
+    } catch (error) {
+      keyword = 'Doh!';
     }
 
     console.log(`${i}: ${keyword}`);
